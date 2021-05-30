78346f00c437be8ec6efbd93c2d6145b625d5c69
Ebu <Ebu@DESKTOP-JAI8NI2>
2021-05-30 18:12:33 +0300

    Auth, Employer and JobSeekers Controllers Added.

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.AuthService;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.Employer;
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	private AuthService authService;
+
+	@Autowired
+	public AuthController(AuthService authService) {
+		super();
+		this.authService = authService;
+	}
+
+	@PostMapping("/registerEmployer")
+	public Result registerEmployer(@RequestBody Employer employer) {
+		return authService.employerRegister(employer);
+	}
+
+	@PostMapping("/registerJobSeeker")
+	public Result registerJobSeekers(@RequestBody JobSeeker jobSeeker) {
+		return authService.jobSeekerRegister(jobSeeker);
+	}
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobSeekersController.java
@@ -0,0 +1,51 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobSeekerService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+
+@RestController
+@RequestMapping("/api/job_seekers")
+public class JobSeekersController {
+
+	private JobSeekerService jobSeekerService;
+
+	@Autowired
+	public JobSeekersController(JobSeekerService jobSeekerService) {
+		super();
+		this.jobSeekerService = jobSeekerService;
+	}
+
+	@GetMapping("/getAll")
+	public DataResult<List<JobSeeker>> getAll() {
+		return this.jobSeekerService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@Valid @RequestBody JobSeeker jobSeeker) {
+		return this.jobSeekerService.add(jobSeeker);
+	}
+	
+	@GetMapping("/getByEmail")
+	public DataResult<JobSeeker> getByEmail(String email){
+		return this.jobSeekerService.getByEmail(email);
+	}
+	
+	@GetMapping("/getByNationalId")
+	public DataResult<JobSeeker> getByNationalId(String nationalId){
+		return this.jobSeekerService.getByNationalId(nationalId);
+	}
+	
+}
